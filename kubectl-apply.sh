a8b9278db7146ccc5543d745aee100fb1101a5dd
Yogendra <yogendra8singh@gmail.com>
2019-05-30 14:52:21 +0530

    Update kubectl-apply.sh

--- kubectl-apply.sh
+++ kubectl-apply.sh
@@ -2,7 +2,7 @@
 # Files are ordered in proper order with needed wait for the dependent custom resource definitions to get initialized.
 # Usage: bash kubectl-apply.sh
 
-export KUBECONFIG=~/.kube/config
+
 
 logSummary(){
     echo ""
